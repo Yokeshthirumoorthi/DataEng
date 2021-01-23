78e24aea97fb702d228b6f4fa770c66adbb4ff3b
Yokesh <yokeshthirumoorthi@gmail.com>
2021-01-22 16:14:01 -0800

    create transaction aware consumer

--- consumer.py
+++ consumer.py
@@ -46,6 +46,7 @@ if __name__ == '__main__':
         'sasl.password': conf['sasl.password'],
         'group.id': 'python_example_group_1',
         'auto.offset.reset': 'earliest',
+        'isolation.level': 'read_committed'
     })
 
     # Subscribe to topic
